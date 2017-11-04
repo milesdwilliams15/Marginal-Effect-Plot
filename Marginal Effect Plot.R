a235720a9782f8371439efa22c68e536214c3b88
Miles Williams <milesdwilliams15@gmail.com>
2017-11-04 17:33:13 -0500

    Updated Code

--- Marginal Effect Plot.R
+++ Marginal Effect Plot.R
@@ -2,25 +2,30 @@
 # ------------------------------------
 # Create Marginal Effect Plot Function
 # ------------------------------------
-meplot <- function(model,var1,var2,ci=.95,xlab=var2,ylab=var1,main="Marginal Effect Plot"){
+meplot <- function(model,var1,var2,ci=.95,
+                   xlab=var2,ylab=paste("Marginal Effect of",var1),
+                   main="Marginal Effect Plot",
+                   me_lty=1,me_lwd=3,me_col="black",
+                   ci_lty=1,ci_lwd=1,ci_col="black",
+                   yint_lty=2,yint_lwd=1,yint_col="black"){
   alpha <- 1-ci
   z <- qnorm(1-alpha/2)
   beta.hat <- coef(model)
   cov <- vcov(model)
   z0 <- seq(min(model$model[,var2],na.rm=T),max(model$model[,var2],na.rm=T),length.out=1000)
-  dy.dx <- beta.hat[var1] + beta.hat[paste(var1,var2,sep=":")]*z0
-  se.dy.dx <- sqrt(cov[var1,var1] + z0^2*cov[paste(var1,var2,sep=":"),paste(var1,var2,sep=":")] + 2*z0*cov[var1,paste(var1,var2,sep=":")])
+  dy.dx <- beta.hat[var1] + beta.hat[length(beta.hat)]*z0
+  se.dy.dx <- sqrt(cov[var1,var1] + z0^2*cov[nrow(cov),ncol(cov)] + 2*z0*cov[var1,ncol(cov)])
   upr <- dy.dx + z*se.dy.dx
   lwr <- dy.dx - z*se.dy.dx
   plot(x=z0, y=dy.dx,type="n",xlim=c(min(z0),max(z0)),
        ylim=c(min(lwr),max(upr)),
        xlab = xlab,
-       ylab = paste("Marginal Effect of",var1),
+       ylab = ylab,
        main = main)
-  lines(z0, dy.dx, lwd = 3)
-  lines(z0, lwr)
-  lines(z0, upr)
-  abline(h=0,lty=2)
+  lines(z0, dy.dx, lwd = me_lwd, lty = me_lty, col = me_col)
+  lines(z0, lwr, lwd = ci_lwd, lty = ci_lty, col = ci_col)
+  lines(z0, upr, lwd = ci_lwd, lty = ci_lty, col = ci_col)
+  abline(h=0,lty=yint_lty,lwd=yint_lwd,col=yint_col)
 }
 
 
